f25470a81e85b1413968007193898d8f071e6f43
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-16 02:27:07 +0430

    Write many variable

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2022;
+
+// log things to console
+console.log("Age ", age, "Year ", year);
+
+age = 30;
+console.log("Age ", age);
+
+const points = 100;
+console.log("Points ", points);
+
+var score = 75;
+console.log("Score ", score);
