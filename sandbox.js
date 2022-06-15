ba065ee276bc8ab64572d59115ec2f3931a2eb09
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-16 03:09:38 +0430

    write many string

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,38 @@
-let age = 25;
-let year = 2022;
+// # Variables
+// let age = 25;
+// let year = 2022;
 
-// log things to console
-console.log("Age ", age, "Year ", year);
+// // log things to console
+// console.log("Age ", age, "Year ", year);
 
-age = 30;
-console.log("Age ", age);
+// age = 30;
+// console.log("Age ", age);
 
-const points = 100;
-console.log("Points ", points);
+// const points = 100;
+// console.log("Points ", points);
 
-var score = 75;
-console.log("Score ", score);
+// var score = 75;
+// console.log("Score ", score);
+
+// # Strings
+
+console.log("hello world");
+
+let email = "mohammadhosseinasadi.p@gmail.com";
+console.log("Email: ", email);
+
+// string concatination
+
+let firstName = "Mohammadhossein";
+let lastName = "Asadi";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[0]);
+
+// string length
+
+// string methods
