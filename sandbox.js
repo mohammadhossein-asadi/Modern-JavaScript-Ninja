ac7588d3411bdaee7857f8f640d69b8362eebf86
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-26 20:38:36 +0430

    Numbers

--- sandbox.js
+++ sandbox.js
@@ -16,34 +16,90 @@
 
 // # Strings
 
-console.log("hello world");
+// console.log("hello world");
 
-let email = "mohammadhosseinasadi.p@gmail.com";
-console.log("Email: ", email);
+// let email = "mohammadhosseinasadi.p@gmail.com";
+// console.log("Email: ", email);
 
-// string concatination
+// // string concatination
 
-let firstName = "Mohammadhossein";
-let lastName = "Asadi";
+// let firstName = "Mohammadhossein";
+// let lastName = "Asadi";
 
-let fullName = firstName + " " + lastName;
-console.log(fullName);
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-// getting characters
+// // getting characters
 
-console.log(fullName[2]);
+// console.log(fullName[2]);
 
-// string length
+// // string length
 
-console.log(fullName.length);
+// console.log(fullName.length);
 
-// string methods
+// // string methods
 
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase()
-console.log(result,fullName);
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase()
+// console.log(result,fullName);
 
-let index = email.indexOf("@")
-let char = email.charAt(22)
+// let index = email.indexOf("@")
+// let char = email.charAt(22)
 
-console.log("Index: ",index,"CharAt: ",char);
+// console.log("Index: ",index,"CharAt: ",char);
+
+// # common string methods
+
+// let email = "mohammadhosseinasadinn.p@gmail.com"
+
+// // let result = email.lastIndexOf("m")
+
+// // let result = email.slice(2,5)
+
+// // let result = email.substring(4,10)
+
+// // let result = email.replace("m","w")
+
+// let result = email.replace("n","w")
+
+// console.log(result);
+
+// # Numbers
+
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operator +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius ** 2;
+
+// order of operation - B I D M A S
+
+// let result = 5 * (10-3)**2
+
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / "hello");
+// console.log(5 * "hello");
+
+// let result = "the blog has " + likes + " likes";
+// console.log(result);
